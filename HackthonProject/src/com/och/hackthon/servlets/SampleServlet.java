30b09d932cc67ad1aa80841775464873524f5cbe
jeroldjwl <1017794273@qq.com>
2016-01-07 22:12:07 +0800

    Add Test page

--- HackthonProject/src/com/och/hackthon/servlets/SampleServlet.java
+++ HackthonProject/src/com/och/hackthon/servlets/SampleServlet.java
@@ -1,31 +1,31 @@
 package com.och.hackthon.servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.och.hackthon.services.UserService;
+import com.och.hackthon.util.DBConnUtil;
+
 public class SampleServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		String name = (String) req.getAttribute("userName");
+		ResultSet rs = null;
+		UserService us = new UserService();
+		Connection conn = DBConnUtil.getConnection();
+		rs = us.getAllUsers(conn);
+		req.getSession().setAttribute("users", rs);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
 	}
 
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.service(req, resp);
-	}
-	
-	
 }
